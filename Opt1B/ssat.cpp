487db1079e8a0fbaadd08d0c1ced1952a28aec0c
Liam Taylor <ltaylor.ulysses@gmail.com>
2016-02-18 17:52:32 -0500

    File IO framework

--- Opt1B/ssat.cpp
+++ Opt1B/ssat.cpp
@@ -1,6 +1,18 @@
+#include <vector>
 
+struct Variable {
+	int name;
+	double probability;
+};
 
 int main(int argv[], int argc) {
+
+	// data structures
+	std::vector<Variable> variables;
+	std::vector<bool> assignments;
+
+	std::vector<std::vector<int>> clauses;
+
 	// file IO
 
 
@@ -11,4 +23,14 @@ int main(int argv[], int argc) {
 
 
 	// report results	
+}
+
+void readSSFile(std::vector<Variable>* variables, 
+				std::vector<std::vector<int>>* clauses)
+{
+	// read in the file
+
+	// fill the argument vectors
+	
+	return;
 }

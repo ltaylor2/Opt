4a2e8195ef5fb4b3893695c44aadc7e0c4113bf2
Liam Taylor <ltaylor.ulysses@gmail.com>
2016-03-08 22:42:20 -0500

    Indent Fix

--- Opt1B/ssat.cpp
+++ Opt1B/ssat.cpp
@@ -63,25 +63,25 @@ int main(int argc, char* argv[])
 
     //User selected naive solution
     if (std::string(argv[1]).compare("n") == 0)
-	directions = SolutionType::naive;
+		directions = SolutionType::naive;
 
     //User selected solution with unit clause propogation
     else if (std::string(argv[1]).compare("u") == 0)
-	directions = SolutionType::unit;
+		directions = SolutionType::unit;
 
     //User selected solution with pure variable elimination
     else if (std::string(argv[1]).compare("p") == 0)
-	directions = SolutionType::pure;
+		directions = SolutionType::pure;
 
     //User selected solution with both unit clause propogation and pure variable elimination
     else if (std::string(argv[1]).compare("b") == 0)
-	directions = SolutionType::both;
+		directions = SolutionType::both;
 
     //User did not put in a valid solution type
     else {
-	std::cout << "---" << argv[1] << "---" << std::endl;
-	std::cout << "Incorrect solving directions. Exiting." << std::endl;
-	return 1;
+		std::cout << "---" << argv[1] << "---" << std::endl;
+		std::cout << "Incorrect solving directions. Exiting." << std::endl;
+		return 1;
     }
 
     //Get filename of user input ssat file
@@ -115,17 +115,17 @@ int main(int argc, char* argv[])
     //Read file in and assign values to variables and clauses
     //If file could not be opened, return 1
     if (readSSATFile(fileName, &variables, &clauses, &varsByClause) == 1) {
-	return 1;
+		return 1;
     }
 
     //Fill assignments such that each variable has an unassigned value
     for (unsigned int i = 0; i < variables.size(); i++) {
-	assignments.push_back(0);
+		assignments.push_back(0);
     }
 	
     //Fill clauseSats such that each clause is unassigned a satisfaction value
     for (unsigned int i = 0; i < clauses.size(); i++) {
-	clauseSats.push_back(0);
+		clauseSats.push_back(0);
     }
 
     //Start solving the SSAT Problem and time it
@@ -156,8 +156,8 @@ int readSSATFile(std::string fileName,
 
     //File could not be opened
     if (!file) {
-	std::cout << "Failed to open file. Exiting." << std::endl;
-	return 1;
+		std::cout << "Failed to open file. Exiting." << std::endl;
+		return 1;
     }
 
     //Line a line in the file
@@ -166,82 +166,81 @@ int readSSATFile(std::string fileName,
     //Iteratue until the loop reaches the end of the file
     while(getline(file, line)) {
 
-	//Loop has reached the "variables" line in the file
-	if (line.compare("variables") == 0) {
+		if (line.compare("variables") == 0) {
 
-	    //Skip a line
-	    getline(file, line);
+	    	//Skip a line
+	    	getline(file, line);
 
-	    //Iterate until all the variables have been read in
-	    while(line.compare("") != 0) {
+	    	//Iterate until all the variables have been read in
+	    	while(line.compare("") != 0) {
 
-		//Convert the line into a string stream object
-		std::stringstream ss(line);
+				//Convert the line into a string stream object
+				std::stringstream ss(line);
 
-		//Name of Variable
-		int varName = 0;
+				//Name of Variable
+				int varName = 0;
 
-		//Probabiltiy of variable being true
-		double varValue = 0.0;
+				//Probabiltiy of variable being true
+				double varValue = 0.0;
 
-		//Assign values to the name of the variable and its probability from the file
-		ss >> varName >> varValue;
+				//Assign values to the name of the variable and its probability from the file
+				ss >> varName >> varValue;
 
-		//Fill variable vector
-		variables->push_back(varValue);
+				//Fill variable vector
+				variables->push_back(varValue);
 
-		//Fill vector of variables that each contain a vector of the clause #s they appear in
-		varsByClause->push_back(std::vector<int>());
+				//Fill vector of variables that each contain a vector of the clause #s they appear in
+				varsByClause->push_back(std::vector<int>());
 
-		//Skip a line
-		getline(file, line);
-	    }
-	}
-
-	//Loop has reached the "variables" line in the file
-	if (line.compare("clauses") == 0) {
-
-	    //Skip line
-	    getline(file, line);
-
-	    //Iterate until all the clauses have been read in
-	    while(line.compare("") != 0) {
-
-		//Create a clause vector for each line
-		std::vector<int> clause;
-
-		//Create stringstream object
-		std::stringstream ss(line);
-
-		//Create int to hold each variable within the clause
-		int literal = variables->size() + 1;
-		ss >> literal;
-
-		//Iterate until the end of the clause
-		while (literal != 0) {
-
-		    //Fill the clause vector
-		    clause.push_back(literal);
-
-		    //Create an int to hold the assignment of each variable
-		    int litSign = 1;
-
-		    //Variable is false
-		    if (literal < 0)
-			litSign = -1;
-
-		    //Fill vector of variables with the clause #s they appear in???
-		    varsByClause->at(abs(literal) - 1).push_back(clauses->size() * litSign);
-		    ss >> literal;
+				//Skip a line
+				getline(file, line);
+	    	}
 		}
 
-		//Fill clauses vector with each clause
-		clauses->push_back(clause);
+		//Loop has reached the "variables" line in the file
+		if (line.compare("clauses") == 0) {
 
-		//Skip a line
-		getline(file,line);
-	    }
-	}
+		    //Skip line
+		    getline(file, line);
+
+		    //Iterate until all the clauses have been read in
+		    while(line.compare("") != 0) {
+
+				//Create a clause vector for each line
+				std::vector<int> clause;
+
+				//Create stringstream object
+				std::stringstream ss(line);
+
+				//Create int to hold each variable within the clause
+				int literal = variables->size() + 1;
+				ss >> literal;
+
+				//Iterate until the end of the clause
+				while (literal != 0) {
+
+				    //Fill the clause vector
+				    clause.push_back(literal);
+
+				    //Create an int to hold the assignment of each variable
+				    int litSign = 1;
+
+				    //Variable is false
+				    if (literal < 0)
+						litSign = -1;
+
+				    //Fill vector of variables with the clause #s they appear in???
+				    varsByClause->at(abs(literal) - 1).push_back(clauses->size() * litSign);
+				    ss >> literal;
+				}
+
+				//Fill clauses vector with each clause
+				clauses->push_back(clause);
+
+				//Skip a line
+				getline(file,line);
+		    }
+		}
     }
 
     return 0;
@@ -264,127 +263,126 @@ double solve(SolutionType directions,
   
     for (unsigned int i = 0; i < clauseSats->size(); i++) {
 
-	//A clause can never be satisfied and thus the entire SSAT problem cannot be satisfied
-	if (clauseSats->at(i) == -1)	        
-	    return 0.0;
+		//A clause can never be satisfied and thus the entire SSAT problem cannot be satisfied
+		if (clauseSats->at(i) == -1)	        
+		    return 0.0;
 
-	//Non-satisfied (but not unsatisfied) clause
-	else if (clauseSats->at(i) == 0) {  
-	    allSat = false;
-	}
+		//Non-satisfied (but not unsatisfied) clause
+		else if (clauseSats->at(i) == 0) {  
+		    allSat = false;
+		}
     }
     //All clauses are satisfied 
     if (allSat)	       
-	return 1.0;
+		return 1.0;
 
 
     //User wants solution to execute unit clause propogation
     if (directions == SolutionType::unit || directions == SolutionType::both) {
 
-	//Create a vector that is a copy of all the clauses
-	std::vector<std::vector<int>> unitCopy(*clauses);
+		//Create a vector that is a copy of all the clauses
+		std::vector<std::vector<int>> unitCopy(*clauses);
 
-	//Create a vector that is a copy of all the clause satisfaction values
-	std::vector<int> unitSats(*clauseSats);
+		//Create a vector that is a copy of all the clause satisfaction values
+		std::vector<int> unitSats(*clauseSats);
 
-	//Create a vector that is a copy of the vector of variables that each contain a vector of the clause #s they appear in
-	std::vector<std::vector<int>> unitVBC(*varsByClause);
+		//Create a vector that is a copy of the vector of variables that each contain a vector of the clause #s they appear in
+		std::vector<std::vector<int>> unitVBC(*varsByClause);
 
-	int unitVar	= 0;
+		int unitVar	= 0;
 
-	//Loop through all the clauses to look for unit clauses
-	for (unsigned int c = 0; c < unitCopy.size(); c++) {
+		//Loop through all the clauses to look for unit clauses
+		for (unsigned int c = 0; c < unitCopy.size(); c++) {
 
-	    //Clause is a unit clause (size 1) and is not satisfied
-	    if (unitCopy[c].size() == 1 && unitSats[c] != 1) {
+		    //Clause is a unit clause (size 1) and is not satisfied
+		    if (unitCopy[c].size() == 1 && unitSats[c] != 1) {
 
-		//Assign unitVar the value of the unit clause variable
-		unitVar = unitCopy[c][0];
-		break;
-	    }
-	}
-    
-	//Unit clause found
-	if (unitVar != 0) {
+				//Assign unitVar the value of the unit clause variable
+				unitVar = unitCopy[c][0];
+				break;
+		    }
+		}
+	    
+		//Unit clause found
+		if (unitVar != 0) {
 
-	    //Fill assignments vector with variable assignment (-1 or 1)
-	    assignments[abs(unitVar)-1] = unitVar / abs(unitVar);
+		    //Fill assignments vector with variable assignment (-1 or 1)
+		    assignments[abs(unitVar)-1] = unitVar / abs(unitVar);
 
-	    //With new state, go through all clauses and determine if they are satisfied or can be shortened
-	    satisfyClauses(abs(unitVar)-1, &unitCopy, &unitSats, &assignments, &unitVBC);
+		    //With new state, go through all clauses and determine if they are satisfied or can be shortened
+		    satisfyClauses(abs(unitVar)-1, &unitCopy, &unitSats, &assignments, &unitVBC);
 
-	    //Solve SSAT with unit clause propogation already conducted
-	    double probSatUnit = solve(directions, variables, &unitCopy, &unitSats, assignments, &unitVBC);
+		    //Solve SSAT with unit clause propogation already conducted
+		    double probSatUnit = solve(directions, variables, &unitCopy, &unitSats, assignments, &unitVBC);
 
-	    //Unit variable is a choice variable
-	    if (variables->at(abs(unitVar)-1) == -1)
-		return probSatUnit;
-	    else {
+		    //Unit variable is a choice variable
+		    if (variables->at(abs(unitVar)-1) == -1)
+				return probSatUnit;
+		    else {
+				//Unit variable is a probabilistic  variable that must be true
+				if (assignments[abs(unitVar)-1] == 1)
+				    return probSatUnit * variables->at(abs(unitVar)-1);
 
-		//Unit variable is a probabilistic  variable that must be true
-		if (assignments[abs(unitVar)-1] == 1)
-		    return probSatUnit * variables->at(abs(unitVar)-1);
-
-		//Unit variable is a probabilistic variable that must be false
-		else
-		    return probSatUnit * (1 - variables->at(abs(unitVar)-1));
-	    }
-	}
+				//Unit variable is a probabilistic variable that must be false
+				else
+				    return probSatUnit * (1 - variables->at(abs(unitVar)-1));
+		    }
+		}
     }
 
     //User wants to eliminate pure variables before solving SSAT
     if (directions == SolutionType::pure || directions == SolutionType::both) {
 
-	//Create a vector that is a copy of all the clauses
-	std::vector<std::vector<int>> pureCopy(*clauses);
+		//Create a vector that is a copy of all the clauses
+		std::vector<std::vector<int>> pureCopy(*clauses);
 
-	//Create a vector that is a copy of all the clause satisfaction values
-	std::vector<int> pureSats(*clauseSats);
+		//Create a vector that is a copy of all the clause satisfaction values
+		std::vector<int> pureSats(*clauseSats);
 
-	//Create a vector that is a copy of the vector of variables that each contain a vector of the clause #s they appear in
-	std::vector<std::vector<int>> pureVBC(*varsByClause);
+		//Create a vector that is a copy of the vector of variables that each contain a vector of the clause #s they appear in
+		std::vector<std::vector<int>> pureVBC(*varsByClause);
 
-	//Initialize an unassigned pure variable
-	int pureVar = -1;
+		//Initialize an unassigned pure variable
+		int pureVar = -1;
 
-	//Loop through all the variables in pureVBC
-	for (int l = 0; l < (signed int)pureVBC.size(); l++) {
+		//Loop through all the variables in pureVBC
+		for (int l = 0; l < (signed int)pureVBC.size(); l++) {
 
-	    //Variable is a chance variable or already assigned
-	    if (variables->at(l) != -1 || assignments[l] != 0)	// only looking at unassigned choice variables
-		continue;
+		    //Variable is a chance variable or already assigned
+		    if (variables->at(l) != -1 || assignments[l] != 0)	// only looking at unassigned choice variables
+			continue;
 
-	    //Loop through each instance of each variable in its respective clauses
-	    for (unsigned int c = 1; c < varsByClause->at(l).size(); c++) {
+		    //Loop through each instance of each variable in its respective clauses
+		    for (unsigned int c = 1; c < varsByClause->at(l).size(); c++) {
 
-		//Assign the pure variable
-		pureVar = l;
+				//Assign the pure variable
+				pureVar = l;
 
-		//Variable is not pure
-		if (!((pureVBC[l][c-1] < 0 && pureVBC[l][c] < 0) || (pureVBC[l][c-1] > 0 && pureVBC[l][c] > 0)) || pureVBC[l].size() <= 1) {
-		    pureVar = -1;
+				//Variable is not pure
+				if (!((pureVBC[l][c-1] < 0 && pureVBC[l][c] < 0) || (pureVBC[l][c-1] > 0 && pureVBC[l][c] > 0)) || pureVBC[l].size() <= 1) {
+				    pureVar = -1;
 
-		    //Move on to next variable
-		    break;
+				    //Move on to next variable
+				    break;
+				}
+		    }
+
+		    //Variable is pure
+		    if (pureVar == l) 
+				break;
 		}
-	    }
 
-	    //Variable is pure
-	    if (pureVar == l) 
-		break;
-	}
+		//Found pure variable
+		if (pureVar != -1) {	// now assign pure var correctly, check for satisfaction, etc.
 
-	//Found pure variable
-	if (pureVar != -1) {	// now assign pure var correctly, check for satisfaction, etc.
+		    //Assign pure variable final value in assignments vector
+		    assignments[pureVar] = pureVBC[pureVar][0] / abs(pureVBC[pureVar][0]);
 
-	    //Assign pure variable final value in assignments vector
-	    assignments[pureVar] = pureVBC[pureVar][0] / abs(pureVBC[pureVar][0]);
-
-	    //Check if all 3clauses are satisfied
-	    satisfyClauses(pureVar, &pureCopy, &pureSats, &assignments, &pureVBC);
-      
-	    return solve(directions, variables, &pureCopy, &pureSats, assignments, &pureVBC);
-	}
+		    //Check if all 3clauses are satisfied
+		    satisfyClauses(pureVar, &pureCopy, &pureSats, &assignments, &pureVBC);
+	      
+		    return solve(directions, variables, &pureCopy, &pureSats, assignments, &pureVBC);
+		}
     }
 
     //There is guaranteed to be a 0 in assignments, because if there was not we would have retunred from allSat == TRUE
@@ -411,8 +409,9 @@ double solve(SolutionType directions,
     double probSatTrue = solve(directions, variables, &trueCopy, &trueSats, assignments, &trueVBC);
 
     if (variables->at(nextVarIndex) == -1) { 	// v is a choice variable
-	return std::max(probSatFalse, probSatTrue);
+		return std::max(probSatFalse, probSatTrue);
     }
+    
     // v is a chance variable
     return probSatTrue * variables->at(nextVarIndex) + probSatFalse * (1 - variables->at(nextVarIndex));
 }
@@ -423,42 +422,42 @@ void satisfyClauses(int varIndex, std::vector<std::vector<int>>* clauses, std::v
     //Loops through all of the clauses
     for (int c = 0; c < (signed int)clauses->size(); c++) {
 
-	//Clause is satisfied
-	if (sats->at(c) == 1)
-	    continue;
+		//Clause is satisfied
+		if (sats->at(c) == 1)
+		    continue;
 
-	//Clause might still be satisfied, Let's check
+		//Clause might still be satisfied, Let's check
 
-	//Loop through each clause
-	for (unsigned int l = 0; l < clauses->at(c).size(); l++) {
+		//Loop through each clause
+		for (unsigned int l = 0; l < clauses->at(c).size(); l++) {
 
-	    //Clause is satisfied, mark this in sats vector
-	    if (clauses->at(c)[l] == (varIndex + 1) * assignments->at(varIndex)) {
-		sats->at(c) = 1;
+		    //Clause is satisfied, mark this in sats vector
+		    if (clauses->at(c)[l] == (varIndex + 1) * assignments->at(varIndex)) {
+				sats->at(c) = 1;
 
-		// Now that a clause is satisfied, erase that clause from the variable's list of active clauses
-		for (unsigned int v = 0; v < varsByClause->size(); v++) {
+				// Now that a clause is satisfied, erase that clause from the variable's list of active clauses
+				for (unsigned int v = 0; v < varsByClause->size(); v++) {
 
-		    //I'm confused
-		    std::vector<int>::iterator it = std::find(varsByClause->at(v).begin(), varsByClause->at(v).end(), c * assignments->at(varIndex));
+				    //I'm confused
+				    std::vector<int>::iterator it = std::find(varsByClause->at(v).begin(), varsByClause->at(v).end(), c * assignments->at(varIndex));
 
-		    // Found variable from vector of 
-		    if (it != varsByClause->at(v).end()) {
-			varsByClause->at(v).erase(it);
+				    // Found variable from vector of 
+				    if (it != varsByClause->at(v).end()) {
+						varsByClause->at(v).erase(it);
+				    }
+				}
+		    }
+		    else if (clauses->at(c)[l] == (varIndex + 1) * assignments->at(varIndex) * -1) {
+				varsByClause->at(varIndex).erase(std::find(varsByClause->at(varIndex).begin(), varsByClause->at(varIndex).end(), c * assignments->at(varIndex) * -1));
+
+				if (clauses->at(c).size() == 1)
+				    sats->at(c) = -1;
+				else {
+				    clauses->at(c).erase(clauses->at(c).begin() + l);
+				    l--;
+				}
 		    }
 		}
-	    }
-	    else if (clauses->at(c)[l] == (varIndex + 1) * assignments->at(varIndex) * -1) {
-		varsByClause->at(varIndex).erase(std::find(varsByClause->at(varIndex).begin(), varsByClause->at(varIndex).end(), c * assignments->at(varIndex) * -1));
-
-		if (clauses->at(c).size() == 1)
-		    sats->at(c) = -1;
-		else {
-		    clauses->at(c).erase(clauses->at(c).begin() + l);
-		    l--;
-		}
-	    }
-	}
     }
 }
 
